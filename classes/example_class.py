919ade0471ba973201668eae58199361b25a8aa7
jaime7981 <jmfernandez1@miaundes.cl>
2023-08-10 22:15:16 -0400

    program entry point and example class

---
+++ classes/example_class.py
@@ -0,0 +1,6 @@
+
+
+class ExampleClass():
+    
+    def __init__(self) -> None:
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print('main program')
+
+
+if __name__ == '__main__':
+    main()
